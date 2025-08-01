ff4c5d9ecc3f3f7bbee1fdac00c0ff18d7d697bb
Thomas Hetzer <hetzer@eualit.de>
2025-08-01 15:17:25 +0200

    Fix german y and z

--- keyboards/keebart/3w6_rgb/keymaps/thhet/keymap.c
+++ keyboards/keebart/3w6_rgb/keymaps/thhet/keymap.c
@@ -24,6 +24,8 @@ enum layers {
 #define OS_LGUI  OSM(MOD_LGUI)
 #define OS_RGUI  OSM(MOD_RGUI)
 
+#define GER_Z    KC_Y    // Z
+#define GER_Y    KC_Z    // Y
 #define GER_ODIA KC_SCLN // Ö
 #define GER_ADIA KC_QUOT // Ä
 #define GER_UDIA KC_LBRC // Ü
@@ -49,10 +51,10 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT_split_3x5_3(
-        KC_Q, KC_W, KC_E,    KC_R,       KC_T,             KC_Z,    KC_U,   KC_I,    KC_O,   KC_P,
-        KC_A, KC_S, KC_D,    KC_F,       KC_G,             KC_H,    KC_J,   KC_K,    KC_L,   GER_ODIA,
-        KC_Y, KC_X, KC_C,    KC_V,       KC_B,             KC_N,    KC_M,   KC_COMM, KC_DOT, GER_MINS,
-                    MO_FUN,  LT_NAV_SPC, MO_NUM,           KC_RSFT, MO_NAV, MO_SYMB
+        KC_Q,  KC_W, KC_E,   KC_R,       KC_T,             GER_Z,   KC_U,   KC_I,    KC_O,   KC_P,
+        KC_A,  KC_S, KC_D,   KC_F,       KC_G,             KC_H,    KC_J,   KC_K,    KC_L,   GER_ODIA,
+        GER_Y, KC_X, KC_C,   KC_V,       KC_B,             KC_N,    KC_M,   KC_COMM, KC_DOT, GER_MINS,
+                     MO_FUN, LT_NAV_SPC, MO_NUM,           KC_RSFT, MO_NAV, MO_SYMB
     ),
 
     [_SYMB] = LAYOUT_split_3x5_3(
